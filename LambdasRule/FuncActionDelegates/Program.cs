c5e01998fda6c5cf5f3f45581996c05661785318
Brian Trager <bptnbs@rit.edu>
2021-02-24 12:39:37 -0500

    Lambda Expressions!

---
+++ LambdasRule/FuncActionDelegates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FuncActionDelegates
+{
+    
+    class Program
+    {
+        public static Action<int, int> DrawLuckyNumber;
+        public static Func<int, int> DrawLuckyMaxNumber;
+        public static Func<List<int>, int> PickANumber;
+        public delegate int MyLuckyNumber(int x);
+        static void Main(string[] args)
+        {
+            //DrawLuckyNumber = delegate (int x, int y)
+            //{
+            //    Random rand = new Random();
+            //    Console.WriteLine(rand.Next(x, y));
+            //};
+            List<int> numbers = new List<int> { 4, 8, 15, 16, 23, 42 };
+            Random rand = new Random();
+
+            DrawLuckyNumber = (x, y) => Console.WriteLine(rand.Next(x, y));
+
+            DrawLuckyMaxNumber = (x) => { return rand.Next(x); };
+
+            PickANumber = (x) => { return x[rand.Next(x.Count - 1)]; };
+   
+            MyLuckyNumber luckyNumber = (x) => { return rand.Next(x); };
+
+            DrawLuckyNumber(25, 75);
+            DrawLuckyNumber(-50, 225);
+            DrawLuckyNumber(0, 1000);
+            DrawLuckyNumber(15, 500);
+
+            Console.WriteLine(DrawLuckyMaxNumber(75));
+            Console.WriteLine(DrawLuckyMaxNumber(225));
+            Console.WriteLine(DrawLuckyMaxNumber(1000));
+            Console.WriteLine(DrawLuckyMaxNumber(500));
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(PickANumber(numbers));
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(luckyNumber(int.MaxValue));
+            Console.ResetColor();
+        }
+    }
+}
---
+++ LambdasRule/LambdasRule/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LambdasRule
+{
+    class Program
+    {
+		delegate bool IsYoungerThan(Student stud, int youngAge);
+		public static void Main()
+		{
+
+            //IsYoungerThan isYoungerThan = delegate (Student s, int age)
+            //{
+            //    return s.Age < age;
+            //};
+
+            IsYoungerThan someMethod = (s, youngAge) => s.Age < youngAge;
+
+			Student stud = new Student() { Age = 25 };
+
+			
+
+			Console.WriteLine(someMethod(stud, 26));
+		}
+
+		//public bool someMethod(Student s, int youngAge)
+		//{
+		//	return s.Age < youngAge;
+		//}
+	}
+
+	class Student
+    {
+		private int age;
+        public int Age { get => age; set => age = value; }
+
+        private int myLuckyNumber;
+
+        public int LuckyNumber
+        {
+            get => myLuckyNumber;
+            set => myLuckyNumber = value;
+        }
+
+
+        //public int Age{
+        //get{ return age; }
+        //set{ age = value; }
+        //}
+    }
+}
